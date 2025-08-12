58f6f717681a2cfdfd6cbce4948c8ef4bd0cdeaf
Santiagoherrella <santiagoherrera@magnetron.com.co>
2025-08-12 10:27:29 -0500

    2

---
+++ auto_push.bat
@@ -0,0 +1,4 @@
+cd "C:\Users\Santiagoherrera\OneDrive - INDUSTRIAS ELECTROMECANICAS MAGNETRON SAS\Magnetron\RedcostosPM\ReduccioncostosPM"
+git add .
+git commit -m "Actualización automática de datos"
+git push
